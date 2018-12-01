483fbb05047843fcb38cfa2824970197adbfa324
Paul van Assen <source@pvanassen.nl>
2018-12-01 17:52:15 +0100

    Output octet stream, for real

--- src/main/kotlin/nl.pvanassen.christmas.tree.animation.common/controller/AnimationController.kt
+++ src/main/kotlin/nl.pvanassen.christmas.tree.animation.common/controller/AnimationController.kt
@@ -9,16 +9,17 @@ abstract class AnimationController {
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
     @Get(value = "/animation/{seconds}/{fps}", processes = ["application/octet-stream"])
-    fun getAnimation(seconds:Int, fps:Int):Single<List<Byte>> {
+    fun getAnimation(seconds:Int, fps:Int):Single<ByteArray> {
         logger.info("Received request for $seconds seconds with $fps fps")
         return Single.just(seconds * fps)
                 .map { getAnimation(it) }
+
     }
 
-    private fun getAnimation(frames:Int):List<Byte> =
+    private fun getAnimation(frames:Int):ByteArray =
         (0..frames).flatMap {
             getFrame().asIterable()
-        }
+        }.toByteArray()
 
 
     abstract fun getFrame(): ByteArray
