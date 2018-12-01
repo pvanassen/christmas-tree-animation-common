e354916b50b8a7bd39a00b4478a18bf687379d77
Paul van Assen <source@pvanassen.nl>
2018-12-01 10:59:28 +0100

    Moving to plain old GET

--- src/main/kotlin/nl.pvanassen.christmas.tree.animation.common/controller/AnimationController.kt
+++ src/main/kotlin/nl.pvanassen.christmas.tree.animation.common/controller/AnimationController.kt
@@ -1,34 +1,27 @@
 package nl.pvanassen.christmas.tree.animation.common.controller
 
-import io.micronaut.websocket.WebSocketSession
-import io.micronaut.websocket.annotation.OnMessage
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
 import io.reactivex.Single
-import io.reactivex.schedulers.Schedulers
-import nl.pvanassen.christmas.tree.animation.common.model.AnimationModel
 import org.slf4j.LoggerFactory
 
+@Controller
 abstract class AnimationController {
-
-    private val subscriber = Schedulers.single()
 
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
-    @OnMessage
-    fun onMessage(
-            message: AnimationModel,
-            session: WebSocketSession) {
-
-        logger.info("Received request for ${message.seconds} seconds")
-
-        val response = (0..message.seconds * message.fps)
-                .map { tick() }
-                .flatMap { it.asIterable() }
-
-        Single.fromPublisher(session.send(response))
-                .toObservable()
-                .subscribeOn(subscriber)
-                .subscribe()
+    @Get("/animation/{seconds}/{fps}")
+    fun getAnimation(seconds:Int, fps:Int):Single<List<Byte>> {
+        logger.info("Received request for $seconds seconds with $fps fps")
+        return Single.just(seconds * fps)
+                .map { getAnimation(it) }
     }
 
-    abstract fun tick(): ByteArray
+    private fun getAnimation(frames:Int):List<Byte> =
+        (0..frames).flatMap {
+            getFrame().asIterable()
+        }
+
+
+    abstract fun getFrame(): ByteArray
 }
--- src/main/kotlin/nl.pvanassen.christmas.tree.animation.common/model/AnimationModel.kt
+++
@@ -1,6 +0,0 @@
-package nl.pvanassen.christmas.tree.animation.common.model
-
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
-
-class AnimationModel @JsonCreator internal constructor(@JsonProperty("seconds") val seconds:Int, @JsonProperty("fps") val fps:Int)
