d5261947ee07ab45ecbc55d3f74c1e46d434e64b
Paul van Assen <source@pvanassen.nl>
2018-12-02 18:30:32 +0100

    Added continuespan

--- src/main/kotlin/nl.pvanassen.christmas.tree.animation.common/controller/AnimationController.kt
+++ src/main/kotlin/nl.pvanassen.christmas.tree.animation.common/controller/AnimationController.kt
@@ -1,6 +1,7 @@
 package nl.pvanassen.christmas.tree.animation.common.controller
 
 import io.micronaut.http.annotation.Get
+import io.micronaut.tracing.annotation.ContinueSpan
 import io.reactivex.Single
 import org.slf4j.LoggerFactory
 
@@ -8,8 +9,9 @@ abstract class AnimationController {
 
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
+    @ContinueSpan
     @Get(value = "/animation/{seconds}/{fps}", processes = ["application/octet-stream"])
-    fun getAnimation(seconds:Int, fps:Int):Single<ByteArray> {
+    open fun getAnimation(seconds:Int, fps:Int):Single<ByteArray> {
         logger.info("Received request for $seconds seconds with $fps fps")
         return Single.just(seconds * fps)
                 .map { getAnimation(it) }
