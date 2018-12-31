4056123d16d0f7f0a4d1e6c1bc2acbf9ee4d26d6
Paul van Assen <source@pvanassen.nl>
2018-12-31 11:23:38 +0100

    Added missing helper

--- src/main/kotlin/nl.pvanassen.christmas.tree.animation.common/model/Animation.kt
+++ src/main/kotlin/nl.pvanassen.christmas.tree.animation.common/model/Animation.kt
@@ -3,13 +3,13 @@ package nl.pvanassen.christmas.tree.animation.common.model
 interface Animation<T> {
     fun getFrame(seed: Long, frame:Int, nsPerFrame:Int): ByteArray
 
-    fun getFrame(seed: Long, frame:Int, nsPerFrame:Int, helper:T): ByteArray {
-        return getFrame(seed, frame, nsPerFrame)
-    }
+    fun getFrame(seed: Long, frame:Int, nsPerFrame:Int, helper:T): ByteArray = getFrame(seed, frame, nsPerFrame)
 
     fun isFixedTimeAnimation():Boolean = false
 
     fun getFixedTimeAnimationFrames():Int = 0
+
+    fun getFixedTimeAnimationFrames(helper:T):Int = getFixedTimeAnimationFrames()
 
     @Suppress("UNCHECKED_CAST")
     fun getHelperObject(): T {
